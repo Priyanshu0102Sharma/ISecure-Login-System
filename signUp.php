d98a03688442422c82c4013a243f32009cb0023b
Priyanshu <priyanshu0102here@gmail.com>
2022-06-23 15:13:03 +0530

    Created structure of sign-up page :p

---
+++ signUp.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+ <link rel="stylesheet" href="partials/styles.css">
+
+    <title>Login System</title>
+</head>
+<body>
+<?php require"partials/navbar.php"; ?>
+<div class="container">
+    <h2 class="text-center">Sign Up to our website</h2>
+
+<!-- form start here -->
+<div class="form1">
+<form aciton="LoginSystem/signUp.php" method="POST" >
+  <div class="mb-3 col-md-6">
+    <label for="username" class="form-label">User Name</label>
+    <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3 col-md-6 ">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3 col-md-6">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="mb-3 col-md-6">
+    <label for="cpassword" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="cpassword" name="cpassword">
+    <div id="password-confirmation" class="form-text">Make sure to enter same password.</div>
+  </div>
+  <!-- <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>  
+</div> -->
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+<!-- form ends here -->
+</div>
+</div>
+
+
+
+
+<!-- bootstrap script link -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
